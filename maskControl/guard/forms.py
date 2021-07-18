4692460eae064b1b2fdeffadf36caebbee7f24d4
elyas21 <elyasabate21@gmail.com>
2021-07-18 14:40:57 +0300

    add recrod somehow working

--- maskControl/guard/forms.py
+++ maskControl/guard/forms.py
@@ -4,23 +4,75 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import current_user
 from maskControl.models import Member
-# id 
-# fname 
-# mname 
-# lname 
-# email  
-# image_file 
+# id
+# fname
+# mname
+# lname
+# email
+# image_file
 
 
 class MemberRegistrationForm(FlaskForm):
     fname = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+                        validators=[DataRequired(), Length(min=2, max=20)])
     mname = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+                        validators=[DataRequired(), Length(min=2, max=20)])
     lname = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+                        validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     submit = SubmitField('Sign Up')
+class AddRecord(FlaskForm):
+    title = StringField('title',
+                        validators=[DataRequired(), Length(min=2, max=60)])
+    note = StringField('note',
+                        validators=[DataRequired(), Length(min=2, max=150)])
+    member_id = StringField('member_id',
+                        validators=[DataRequired()])
+    submit = SubmitField('Record')
 
- 
+
+
+class UpdateMember(FlaskForm):
+    fname = StringField('Username',
+                        validators=[DataRequired(), Length(min=2, max=20)])
+    mname = StringField('Username',
+                        validators=[DataRequired(), Length(min=2, max=20)])
+    lname = StringField('Username',
+                        validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[
+                        FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    # def validate_username(self, username):
+    #     if username.data != current_user.username:
+    #         user = User.query.filter_by(username=username.data).first()
+    #         if user:
+    #             raise ValidationError(
+    #                 'That username is taken. Please choose a different one.')
+
+    # def validate_email(self, email):
+    #     if email.data != current_user.email:
+    #         user = Member.query.filter_by(email=email.data).first()
+    #         if user:
+    #             raise ValidationError(
+    #                 'That email is taken. Please choose a different one.')
+    # def validate_id(self, id):
+    #     if id.data != current_user.id:
+    #         user = Member.query.filter_by(id=id.data).first()
+    #         if user:
+    #             raise ValidationError('That id is taken. Please choose a different one.')
+
+
+class SearchMember(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
+    # def validate_email(self, email):
+    #     if email.data != current_user.email:
+    #         user = Member.query.filter_by(email=email.data).first()
+    #         if user:
+    #             raise ValidationError(
+    #                 'That email is not exist. Please choose a different one.')
--- maskControl/guard/routes.py
+++ maskControl/guard/routes.py
@@ -8,11 +8,12 @@ from maskControl import redis_client
 import datetime
 from flask import Response, render_template, url_for, flash, redirect, request, Blueprint
 
-from maskControl.guard.forms import MemberRegistrationForm
+from maskControl.guard.forms import MemberRegistrationForm, UpdateMember, SearchMember, AddRecord
 from maskControl import db
 from maskControl.models import Member
+from maskControl.models import Record
 from flask_login import login_user, current_user, logout_user, login_required
-
+from maskControl.users.utils import save_picture
 
 guard = Blueprint('guard', __name__, template_folder='templates')
 
@@ -28,9 +29,40 @@ def stream(camId):
     return render_template('stream.html', cameraId=camId)
 
 
-@guard.route("/record-disobdient")
-def record_disobdient():
-    return render_template('record-disobdient.html', title='record-disobdient')
+@guard.route("/record-disobdient", methods=['POST', 'GET'])
+@guard.route("/record-disobdient/<int:id>", methods=['POST', 'GET'])
+def record_disobdient(id=None):
+    if id is not None:
+        records = Record.query.filter_by(member_id=id).all()
+
+    addRecordForm = AddRecord()
+    _note = request.form.get('note')
+    _title = request.form.get('title')
+    _form_member_id = request.form.get('member_id')
+    _member_id = id if id is not None else _form_member_id
+    print(_title)
+    print(_note)
+    print(_member_id )
+    if addRecordForm.is_submitted() and _note and request.method == 'POST':
+        record = Record(title=_title, note=_note, member_id=_member_id)
+        print(record)
+        db.session.add(record)
+        db.session.commit()
+        flash(
+            f'record has been added succsesfuly', 'success')
+        addRecordForm.title.data = ''
+        addRecordForm.note.data = ''
+        addRecordForm.member_id.data = ''
+
+        # return redirect(url_for('gurad.registerrecord'))
+        if id is None:
+            records = Record.query.filter_by(member_id=_member_id).all()
+        return render_template('record-disobdient.html', addRecordForm=addRecordForm, title='record-disobdient', records=records)
+
+    if id is not None:
+        return render_template('record-disobdient.html', addRecordForm=addRecordForm, title='record-disobdient', records=records)
+
+    return render_template('record-disobdient.html', addRecordForm=addRecordForm, title='record-disobdient')
 
 
 def gen(camId):
@@ -96,5 +128,74 @@ def registerMember():
 
 @guard.route("/view-members", methods=['GET'])
 @login_required
-def view_member():
-    return render_template('view-members.html')
+def view_members():
+    page = request.args.get('page', 1, type=int)
+    members = Member.query.order_by(
+        Member.id.desc()).paginate(page=page, per_page=5)
+    print(members)
+    return render_template('view-members.html', members=members)
+
+
+@guard.route("/view-member/<int:member_id>", methods=['GET', 'POST'])
+@guard.route("/search-member",  methods=['GET', 'POST'])
+@login_required
+def view_member(member_id=None):
+
+    form = UpdateMember()
+    searchform = SearchMember()
+
+    _fname = request.form.get('fname')
+    _mname = request.form.get('mname')
+    _lname = request.form.get('lname')
+    _email = request.form.get('email')
+    _searchemail = request.args.get('email')
+    print(_searchemail)
+    print(form)
+    # _id = request.form.get('id')
+
+    if form.validate_on_submit() and _fname and _mname and _lname and _email and member_id and request.method == 'POST':
+        member = Member.query.filter_by(id=member_id).first()
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            member.image_file = picture_file
+        member.fname = _fname
+        member.mname = _mname
+        member.lname = _lname
+        member.email = _email
+        db.session.commit()
+        form.fname.data = ''
+        form.mname.data = ''
+        form.lname.data = ''
+        form.email.data = ''
+        form.email.data = ''
+        flash('Your account has been updated!', 'success')
+        return render_template('view-member.html', form=form, searchform=searchform, member_id=member_id)
+
+    elif _searchemail and request.method == 'GET':
+        print('in \n'*3)
+        member = Member.query.filter_by(email=_searchemail).first()
+        print(member)
+        form.email.data = member.email
+        member_id = member.id
+        return render_template('view-member.html', form=form, searchform=searchform, member_id=member_id)
+
+    elif request.method == 'GET' and member_id:
+        member = Member.query.filter_by(id=member_id).first()
+        print(member)
+        form.fname.data = member.fname
+        form.mname.data = member.mname
+        form.lname.data = member.lname
+        form.email.data = member.email
+
+        return render_template('view-member.html', form=form, searchform=searchform, member_id=member_id)
+
+    elif request.method == 'GET':
+        print('out \n'*3)
+        return render_template('view-member.html', form=form, searchform=searchform, member_id=member_id)
+
+        # image_file = url_for(
+        # 'static', filename='profile_pics/' + current_user.image_file)
+        # return render_template('account.html', title='Account',
+        #                 image_file=image_file, form=form)
+    else:
+        return render_template('internal-server-error.html')
